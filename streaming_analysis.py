958636a17a943e3f95f1dfa0053348d17484ed91
tiafluellen <tiafluellen@abbey.bac.edu>
2025-11-09 13:30:25 -0500

    Update streaming_analysis.py

--- streaming_analysis.py
+++ streaming_analysis.py
@@ -1,1 +1,8 @@
-# Create your code here
+import matplotlib.pyplot as plt
+
+# Step 1: Read CSV file
+df = pd.read_csv('personal_listening_history.csv')
+
+# Optional: preview the data
+print(df.head())
+ here
